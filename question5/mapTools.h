0926ff2dc8b8583d86d2cc0826ea71acbe7d3e62
Luke-J-Miller <111100132+Luke-J-Miller@users.noreply.github.com>
2022-11-25 17:05:51 -0600

    Update mapTools.h

--- question5/mapTools.h
+++ question5/mapTools.h
@@ -3,13 +3,15 @@
 #include <string>
 #include <iostream>
 
-
+//created a class for the map to avoid verbose functions in main
 class stateDataMap{
 
 public:
+	//this function adds new key-value pairs to the map
 	void addKeyVal(std::string key, std::string value) {
 		myStateDataMap.insert(std::pair<std::string, std::string>(key, value));
 	}
+	//this function iterates through the map and prints the key-value pairs
 	void printMap() {
 		std::map <std::string, std::string>::iterator itr;
 		std::cout << "My State Data Map Contents: " << std::endl;
@@ -18,9 +20,11 @@ public:
 		}
 		std::cout << std::endl;
 	}
+	//this function returns a value give a key
 	std::string getVal(std::string key) {
 		return myStateDataMap[key];
 	}
+	//this function edits a value given a key
 	void changeVal(std::string key, std::string val) {
 		myStateDataMap[key] = val;
 	}
