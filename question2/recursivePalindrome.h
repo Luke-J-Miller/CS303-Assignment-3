bbebcdc5692ecdc31f4ebae62dfc098d7ea260b8
Luke-J-Miller <111100132+Luke-J-Miller@users.noreply.github.com>
2022-11-25 17:00:29 -0600

    Update recursivePalindrome.h

--- question2/recursivePalindrome.h
+++ question2/recursivePalindrome.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <vector>
 
+//recursive function with base case of size == 1||0 returning true;  
+//if front and last elements match, the string without those elements is fed to recursive function
+//else it returns false
 bool isPalindrome(std::string potentialPalindrome) {
 	if (potentialPalindrome.size() < 2) {
 		return true;
